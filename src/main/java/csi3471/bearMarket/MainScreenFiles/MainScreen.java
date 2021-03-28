017579e8fe44d702080954fdf83e888dbc5b3d3c
Richard-Hutcheson <richardhutch80@gmail.com>
2021-03-27 21:17:21 -0500

    adjusted main

---
+++ src/main/java/csi3471/bearMarket/MainScreenFiles/MainScreen.java
@@ -0,0 +1,20 @@
+package csi3471.bearMarket;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainScreen extends JPanel implements ActionListener {
+    private  JFrame frame;
+    MainScreen(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public static void createAndShowLogin(){
+        
+    }
+}
