45c092e40728abe0cdb9a49e78ee28330c086062
Nitsua365 <austinblanchard@hotmail.com>
2021-03-28 18:38:34 -0500

    DeletePostWindow Commit v1.2

--- src/main/java/csi3471/bearMarket/DeletePostWindow.java
+++ src/main/java/csi3471/bearMarket/DeletePostWindow.java
@@ -15,26 +15,6 @@ public class DeletePostWindow implements ActionListener {
         frame.setPreferredSize(new Dimension(400, 150));
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setVisible(true);
-        frame.setLayout(new GridBagLayout());
-
-//        GridBagConstraints c = new GridBagConstraints();
-//
-//        JLabel prompt = new JLabel("Are you sure?");
-//        c.gridx = 1;
-//        c.gridy = 0;
-//        frame.add(prompt, c);
-//
-//        JButton confirmYes = new JButton("Yes");
-//        c.gridx = 0;
-//        c.gridy = 1;
-//        confirmYes.setPreferredSize(new Dimension(150, 50));
-//        frame.add(confirmYes, c);
-//
-//        JButton confirmNo = new JButton("Cancel");
-//        c.gridx = 2;
-//        c.gridy = 1;
-//        confirmNo.setPreferredSize(new Dimension(150, 50));
-//        frame.add(confirmNo, c);
 
         // save the option result
         int dialogResult = JOptionPane.showConfirmDialog(frame, "Are you sure?", "Delete?", JOptionPane.YES_NO_OPTION);
