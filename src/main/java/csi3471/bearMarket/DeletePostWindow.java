c4374b04aab56c5666725c671d3136a1e9c71991
Nitsua365 <austinblanchard@hotmail.com>
2021-03-28 18:35:49 -0500

    DeletePostWindow Commit v1.1

--- src/main/java/csi3471/bearMarket/DeletePostWindow.java
+++ src/main/java/csi3471/bearMarket/DeletePostWindow.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionListener;
 public class DeletePostWindow implements ActionListener {
 
     JFrame frame;
-    JButton confirmYes, confirmNo;
+
 
     public DeletePostWindow() {
         frame = new JFrame();
@@ -17,35 +17,40 @@ public class DeletePostWindow implements ActionListener {
         frame.setVisible(true);
         frame.setLayout(new GridBagLayout());
 
-        GridBagConstraints c = new GridBagConstraints();
+//        GridBagConstraints c = new GridBagConstraints();
+//
+//        JLabel prompt = new JLabel("Are you sure?");
+//        c.gridx = 1;
+//        c.gridy = 0;
+//        frame.add(prompt, c);
+//
+//        JButton confirmYes = new JButton("Yes");
+//        c.gridx = 0;
+//        c.gridy = 1;
+//        confirmYes.setPreferredSize(new Dimension(150, 50));
+//        frame.add(confirmYes, c);
+//
+//        JButton confirmNo = new JButton("Cancel");
+//        c.gridx = 2;
+//        c.gridy = 1;
+//        confirmNo.setPreferredSize(new Dimension(150, 50));
+//        frame.add(confirmNo, c);
 
-        JLabel prompt = new JLabel("Are you sure?");
-        c.gridx = 1;
-        c.gridy = 0;
-        frame.add(prompt, c);
+        // save the option result
+        int dialogResult = JOptionPane.showConfirmDialog(frame, "Are you sure?", "Delete?", JOptionPane.YES_NO_OPTION);
 
-        JButton confirmYes = new JButton("Yes");
-        c.gridx = 0;
-        c.gridy = 1;
-        confirmYes.setPreferredSize(new Dimension(150, 50));
-        frame.add(confirmYes, c);
-
-        JButton confirmNo = new JButton("Cancel");
-        c.gridx = 2;
-        c.gridy = 1;
-        confirmNo.setPreferredSize(new Dimension(150, 50));
-        frame.add(confirmNo, c);
+        if (dialogResult == JOptionPane.YES_OPTION) {
+            // WIP
+        }
+        else if (dialogResult == JOptionPane.NO_OPTION) {
+            // WIP
+        }
 
         frame.pack();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == confirmYes) {
 
-        }
-        else if (e.getSource() == confirmNo) {
-
-        }
     }
 }
