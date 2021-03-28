cb01d940022e0b03a53da3fb7935f802870eaa19
Richard Hutcheson <richardhutch80@gmail.com>
2021-03-28 03:08:46 -0500

    added menu bar separators

--- src/main/java/csi3471/bearMarket/MainScreenFiles/MainScreen.java
+++ src/main/java/csi3471/bearMarket/MainScreenFiles/MainScreen.java
@@ -15,36 +15,47 @@ public class MainScreen extends JPanel implements ActionListener {
     private JMenu createPosting;
     private JMenu exit;
     MainScreen(){
+        //MENUBAR
         JMenuBar menuBar = new JMenuBar();
-        final JMenu editAccount = new JMenu("Edit Account");
+        JMenu editAccount = new JMenu("Edit Account");
         JMenu purchaseHistory = new JMenu("Purchase History");
         JMenu currentlySelling = new JMenu("Currently Selling");
         JMenu createPosting = new JMenu("Create Posting");
-        JMenu exit = new JMenu("EXIT");
+        JMenu exit = new JMenu("Exit");
+        //SEPARATORS
+        JMenu space1 = new JMenu("    ");
+        space1.setEnabled(false);
+        JMenu space2 = new JMenu("    ");
+        space2.setEnabled(false);
+        JMenu space3 = new JMenu("    ");
+        space3.setEnabled(false);
+        JMenu space4 = new JMenu("    ");
+        space4.setEnabled(false);
         exit.addMenuListener(new MenuListener() {
             @Override
             public void menuSelected(MenuEvent e) {
                 int dialogResult = JOptionPane.showConfirmDialog(null, "Exit Application?", "Exit", JOptionPane.YES_NO_OPTION);
-                if (dialogResult == JOptionPane.YES_OPTION){
-                    frame.dispose();
-                }
+                //user selects 'yes' option to exit application
+                if (dialogResult == JOptionPane.YES_OPTION){ frame.dispose(); }
             }
-
             @Override
             public void menuDeselected(MenuEvent e) {}
 
             @Override
             public void menuCanceled(MenuEvent e) {}
         });
-
-
-
         menuBar.add(editAccount);
+        menuBar.add(space1);
         menuBar.add(purchaseHistory);
+        menuBar.add(space2);
         menuBar.add(currentlySelling);
+        menuBar.add(space3);
         menuBar.add(createPosting);
+        menuBar.add(space4);
         menuBar.add(exit);
-        add(menuBar);
+
+        
+        frame.setJMenuBar(menuBar);
 
     }
 
